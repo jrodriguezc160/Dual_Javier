6eb57712cba1885755cde11be5decd1678cc37ff
Javier Rodríguez Cortés <jrcortes02102005@educarex.es>
2023-10-16 10:45:07 +0200

    Update dom.js

--- Semana 3 - del 16 al 20 de octubre/dom.js
+++ Semana 3 - del 16 al 20 de octubre/dom.js
@@ -38,3 +38,32 @@ console.log(document.querySelectorAll("#menu li"));
 
 // Curso JavaScript: 63. DOM: Atributos y Data-Attributes
 
+console.warn("***** ATRIBUTOS & DATA-ATTRIBUTES *****");
+
+// document y getAttribute parecen apuntar al mismo sitio, pero no es así
+console.log(document.documentElement.lang); // en
+console.log(document.documentElement.getAttribute("lang")); // en
+
+console.log(document.querySelector(".link-dom").href); // http://127.0.0.1:5500/dom.html
+console.log(document.querySelector(".link-dom").getAttribute("href")); // dom.html
+
+
+// Al igual que existe getAttribute, también existe setAttribute
+document.documentElement.lang = "es";
+console.log(document.documentElement.lang);
+
+document.documentElement.setAttribute("lang", "fr");
+console.log(document.documentElement.lang);
+
+
+// Trabajamos con setAttribute...
+const $linkDOM = document.querySelector(".link-dom"); // Declaramos constantes del DOM con un $ delante
+
+$linkDOM.setAttribute("target", "_blank");
+$linkDOM.setAttribute("href", "https://youtube.com/jonmircha");
+
+$linkDOM.setAttribute("rel", "noopener");
+console.log($linkDOM.hasAttribute("rel")); // true
+
+$linkDOM.removeAttribute("rel");
+console.log($linkDOM.hasAttribute("rel")); // false
