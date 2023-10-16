730159fb4a28b1b5dba510f8da5c7a6668b92c09
Javier Rodríguez Cortés <jrcortes02102005@educarex.es>
2023-10-16 09:25:26 +0200

    Create dom.js

---
+++ Semana 3 - del 16 al 20 de octubre/dom.js
@@ -0,0 +1,25 @@
+// Curso JavaScript: 60. DOM: Introducción
+
+console.warn("***** ELEMENTOS DEL DOM *****");
+
+console.log(document);
+console.log(document.head);
+console.log(document.body);
+console.log(document.documentElement);
+console.log(document.doctype);
+console.log(document.characterSet);
+console.log(document.title);
+console.log(document.links);
+console.log(document.images);
+console.log(document.forms);
+console.log(document.styleSheets);
+console.log(document.scripts);
+
+setTimeout(() => {
+  console.log(document.getSelection().toString());
+}, 3000);
+
+
+
+// Curso JavaScript: 61. DOM: Introducción
+
