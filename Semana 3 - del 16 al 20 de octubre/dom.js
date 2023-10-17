09598a6aa1d0d56d2427014df2027c41bcfbafaf
Javier Rodríguez Cortés <jrcortes02102005@educarex.es>
2023-10-17 08:51:15 +0200

    Update dom.js

--- Semana 3 - del 16 al 20 de octubre/dom.js
+++ Semana 3 - del 16 al 20 de octubre/dom.js
@@ -160,3 +160,30 @@ console.log($card.classList.contains("rotate-45"));
 $card.classList.add("opacity-80", "sepia");
 $card.classList.remove("opacity-80", "sepia");
 $card.classList.toggle("opacity-80", "sepia");
+
+
+
+// Curso JavaScript: 66. DOM: Texto y HTML
+
+console.warn("***** TEXTO & HTML *****");
+
+const $whatIsDOM = document.getElementById("que-es");
+
+let text = `
+    <p>
+      El Modelo de Objetos del Documento (<b><i>DOM - Document Object Model</i></b>) es un API para documentos HTML y XML.
+    </p>
+    <p>
+      Éste proveé una representación estructural del documento, permitiendo modificar su contenido y presentación visual mediante código JS.
+    </p>
+    <p>
+      <mark>El DOM no es parte de la especificación de JavaScript, es una API para los navegadores.</mark>
+    </p>
+  `;
+
+// $whatIsDOM.innerText = text;
+$whatIsDOM.textContent = text;
+$whatIsDOM.innerHTML = text;
+$whatIsDOM.outerHTML = text;
+
+
