2269132c90f615ab1a382247656118083225751e
jrodriguezc160 <jrcortes02102005@educarex.es>
2023-10-25 13:06:40 +0200

    search filters

--- Ejercicios-DOM/javascript/filtro_busquedas.js
+++ Ejercicios-DOM/javascript/filtro_busquedas.js
@@ -1,14 +1,14 @@
 const d = document;
 
 export default function searchFilters(input, selector) {
-  d.addEventListener("keydown", (e) => {
+  d.addEventListener("input", (e) => {
     if (e.target.matches(input)) {
       console.log(e.target.value);
-      d.querySelectorAll(selector).forEach((el) => {
+      d.querySelectorAll(selector).forEach((el) =>
         el.textContent.toLowerCase().includes(e.target.value)
           ? el.classList.remove("filter")
-          : el.classList.add("filter");
-      });
+          : el.classList.add("filter")
+      );
     }
   });
 }
