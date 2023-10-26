f66f621e2107c82a4cc4ee3263ea20bcc961af86
jrodriguezc160 <jrcortes02102005@educarex.es>
2023-10-26 10:30:30 +0200

    sadfasdf

--- Ejercicios-DOM/javascript/scroll_espia.js
+++ Ejercicios-DOM/javascript/scroll_espia.js
@@ -4,7 +4,16 @@ export default function scrollSpy() {
   const $sections = d.querySelectorAll("section[data-scroll-spy]");
 
   const cb = (entries) => {
-    console.log("entries", entries);
+    // console.log("entries", entries);
+
+    entries.forEach((entry) => {
+      // console.log(entry)
+      const id = entry.target.getAttribute("id");
+      if (entry.isIntersecting) {
+        d.querySelector(`a[data-scroll-spy][href="#${id}"]`);
+      } else {
+      }
+    });
   };
 
   const observer = new IntersectionObserver(cb, {});
