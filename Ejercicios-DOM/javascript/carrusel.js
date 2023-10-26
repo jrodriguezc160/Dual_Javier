ac1bc8ed83df8ff8db68150e2f00a58fe0b5622b
jrodriguezc160 <jrcortes02102005@educarex.es>
2023-10-26 09:18:31 +0200

    sfsdgf

--- Ejercicios-DOM/javascript/carrusel.js
+++ Ejercicios-DOM/javascript/carrusel.js
@@ -1,21 +1,37 @@
 const d = document;
 
 export default function slider() {
-  const $nextBtn = d.querySelector(".slider-btns.next"),
-  $prevBtn = d.querySelector(".slider-btns.prev"),
-  $slides = d.querySelectorAll(".slide");
+  const $nextBtn = d.querySelector(".slider-btns .next"),
+    $prevBtn = d.querySelector(".slider-btns .prev"),
+    $slides = d.querySelectorAll(".slider-slide");
 
   let i = 0;
 
-  d.addEventListener("click", e => {
+  d.addEventListener("click", (e) => {
     if (e.target === $prevBtn) {
-        e.preventDefault();
-        $slides[i].classList.remove("active");
-        i--;
+      e.preventDefault();
+      $slides[i].classList.remove("active");
+      i--;
 
-        if (i<0) {i = $slides.length - 1;}
+      if (i < 0) {
+        i = $slides.length - 1;
+      }
 
-        $slides[i].classList.add("active")
+      $slides[i].classList.add("active");
     }
-  })
+  });
+
+  d.addEventListener("click", (e) => {
+    if (e.target === $nextBtn) {
+      e.preventDefault();
+      $slides[i].classList.remove("active");
+      i++;
+
+      if (i >= $slides.length) {
+        i = 0;
+      }
+
+      $slides[i].classList.add("active");
+    }
+  });
 }
