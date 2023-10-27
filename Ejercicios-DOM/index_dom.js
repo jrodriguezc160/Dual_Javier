72e3910ec8530399c0bd0e0db1fcc0f4110b184b
jrodriguezc160 <jrcortes02102005@educarex.es>
2023-10-27 10:42:45 +0200

    asdfasdfdsf

--- Ejercicios-DOM/index_dom.js
+++ Ejercicios-DOM/index_dom.js
@@ -45,8 +45,8 @@ d.addEventListener("DOMContentLoaded", (e) => {
     "(min-width: 1024px)",
     `<a href="https://www.google.com/maps/place/El+Rodeo/@39.4698717,-6.3818323,15z/data=!4m6!3m5!1s0xd15dfce0eb5ad23:0xa1895cab1b17d123!8m2!3d39.4663324!4d-6.3708228!16s%2Fg%2F11b6hw4498?entry=ttu 
     target="_blank">Ver mapa</a>`,
-    `<div style="border-radius: 1rem; overflow: hidden; height="315"; width="560"><iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d12320.422056324322!2d-6.3758094!3d39.46694495!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0xd15dfce0eb5ad23%3A0xa1895cab1b17d123!2sEl%20Rodeo!5e0!3m2!1ses!2ses!4v1698133865661!5m2!1ses!2ses" 
-    width="560" height="315" style="border:0;" allowfullscreen="" loading="lazy" 
+    `<div style="border-radius: 1rem; overflow: hidden; max-height="315px"; max-width="560px"";"><iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d12320.422056324322!2d-6.3758094!3d39.46694495!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0xd15dfce0eb5ad23%3A0xa1895cab1b17d123!2sEl%20Rodeo!5e0!3m2!1ses!2ses!4v1698133865661!5m2!1ses!2ses" 
+    width="560px" height="315px" style="border:0; border-radius: 1rem" allowfullscreen="" loading="lazy" 
     referrerpolicy="no-referrer-when-downgrade"></iframe></div>`
   );
 
